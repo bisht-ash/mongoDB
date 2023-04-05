b29e094d5fff46cab85c9507a5b5eb35d3a30fa2
Aswat Singh Bisht <aswat.b@sigmoidanalytics.com>
2023-04-05 15:18:48 +0530

    add data

--- dataBase.py
+++ dataBase.py
@@ -1,8 +1,17 @@
 import pymongo
-
+import subprocess
+def load():
+  subprocess.call(["mongoimport","-d","mflix","-c","movies","--file","./Data/movies.json"])
+  subprocess.call(["mongoimport","-d","mflix","-c","comments","--file","./Data/comments.json"])
+  subprocess.call(["mongoimport","-d","mflix","-c","theaters","--file","./Data/theaters.json"])
+  
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 
-mydb = myclient["mflix"]
+try:
+  mydb = myclient["mflix"]
+except:
+  load()
+  mydb = myclient["mflix"]
 
 commentsCollection=mydb["comments"]
 moviesCollection=mydb["movies"]
